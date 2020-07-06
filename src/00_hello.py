d867bcf7b031a82a176f1fe8c18dbd6ddd22cbb3
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-07-06 10:10:16 -0700

    completed list tasks

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,2 +1,11 @@
 # Print "Hello, world!" to your terminal
 print("Hello World!")
+
+name = "Nick"
+
+# String interpolation
+
+if name != "Nick":
+    print("Hey, you aren't Nick!")
+else:
+    print(f"Hello {name}")
